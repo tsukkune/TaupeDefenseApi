8126b11d0ebbf76fbdfb45b3a67826c1f1fbea4f
unknown <nico.depero@hotmail.fr>
2018-11-22 15:13:28 +0100

    route create users

---
+++ server.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const errorHandler = require('errorhandler');
+
+//Configure mongoose's promise to global promise
+mongoose.promise = global.Promise;
+
+//Configure isProduction variable
+const isProduction = process.env.NODE_ENV === 'production';
+
+//Initiate our app
+const app = express();
+
+//Configure our app
+app.use(cors());
+app.use(require('morgan')('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({ secret: 'passport-tutorial', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false }));
+
+if(!isProduction) {
+  app.use(errorHandler());
+}
+
+//Configure Mongoose
+var url = "mongodb://localhost/dbtaupe";
+
+mongoose.connect(url,{ useNewUrlParser: true });
+mongoose.set('debug', true);
+
+//Models & routes
+require('./models/Users.js');
+require('./config/passport');
+app.use(require('./routes'));
+
+//Error handlers & middlewares
+if(!isProduction) {
+  app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+
+    res.json({
+      errors: {
+        message: err.message,
+        error: err,
+      },
+    });
+  });
+}
+
+if(isProduction) {
+  app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+
+    res.json({
+      errors: {
+        message: err.message,
+        error: {},
+      },
+    });
+  });
+}
+
+app.listen(8000, () => console.log('Server running on http://localhost:8000/'));
